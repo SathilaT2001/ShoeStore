e5dc38a680f215b753eb683215b9c28efd04d290
AyeshaL2000 <lakmaliayesha83@gmail.com>
2024-04-29 13:06:07 +0530

    Connect register form to Database

---
+++ ShoeStore/src/java/newpackage/UserDatabase.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+import java.sql.*;
+
+public class UserDatabase {
+    Connection con ;
+
+    public UserDatabase(Connection con) {
+        this.con = con;
+    }
+    
+    //for register user 
+    public boolean saveUser(User user){
+        boolean set = false;
+        try{
+            //Insert register data to database
+            String query = "insert into user(name,email,password) values(?,?,?)";
+           
+           PreparedStatement pt = this.con.prepareStatement(query);
+           pt.setString(1, user.getName());
+           pt.setString(2, user.getEmail());
+           pt.setString(3, user.getPassword());
+           
+           pt.executeUpdate();
+           set = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return set;
+    }
+}
